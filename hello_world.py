018f815cf6b6ea02dffd55fa2b97d3a364704bda
xin053 <13207130066.cool@163.com>
2016-06-11 13:10:15 +0800

    first commit

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Copyright©2016.All rights reserved by xin053.
+    ------------------------------------------------------------------------------------------------
+    FileName: hello_world.py
+    Author: xin053
+    Version: 1.0
+    Time: 2016-06-11 09:50:47
+    License: Apache License 2.0
+    Description: Hello World
+    Example:
+    ------------------------------------------------------------------------------------------------
+"""
+
+
+if __name__ == '__main__':
+    print("Hello World")
+
+
