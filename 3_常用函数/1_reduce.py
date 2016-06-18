a3d389fec1f281c41e7436fc6323b1f5b4341517
xin053 <13207130066.cool@163.com>
2016-06-18 23:31:57 +0800

    update

---
+++ 3_常用函数/1_reduce.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Copyright©2016.All rights reserved by xin053.
+    ------------------------------------------------------------------------------------------------
+    FileName: 1_reduce.py
+    Author: xin053
+    Version: 1.0
+    Time: 2016-06-18 23:16:28
+    License: Apache License 2.0
+    Description: reduce函数使用
+    Example:
+    ------------------------------------------------------------------------------------------------
+"""
+
+
+from functools import reduce
+
+if __name__ == '__main__':
+    product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
+    print(product)
+
+
